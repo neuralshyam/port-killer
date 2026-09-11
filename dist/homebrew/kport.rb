9965c46e5a456cf438aac11129fd702b3126c340
neuralshyam <neuralshyam@example.com>
2026-09-11 08:01:11 +0530

    chore(homebrew): update formula hashes for v0.2.1

--- dist/homebrew/kport.rb
+++ dist/homebrew/kport.rb
@@ -1,31 +1,31 @@
 class Kport < Formula
   desc "⚡ Ultra-fast CLI & TUI to hunt down and kill processes hogging network ports"
   homepage "https://neuralshyam.github.io/kport/"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/neuralshyam/kport/releases/download/v0.2.0/port-killer-aarch64-apple-darwin.tar.gz"
-      sha256 "0f661e53b9b613197a3a8716b1dc95171e21b8b201a4e107dfca0cf67fcb3602"
+      url "https://github.com/neuralshyam/kport/releases/download/v0.2.1/kport-aarch64-apple-darwin.tar.gz"
+      sha256 "18ecd1d497e6b49a37e4aface670c60eafde5e64dfc18e0cd0a6a4842355986e"
     else
-      url "https://github.com/neuralshyam/kport/releases/download/v0.2.0/port-killer-x86_64-apple-darwin.tar.gz"
-      sha256 "95edfe6a787b8c0496328f95c478a87b7a63ce73f8d689622d148e658ae24068"
+      url "https://github.com/neuralshyam/kport/releases/download/v0.2.1/kport-x86_64-apple-darwin.tar.gz"
+      sha256 "e872abd66b92dd75c646b08c9d02b29e1a20ac95a1696146e99e94eb73c12586"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/neuralshyam/kport/releases/download/v0.2.0/port-killer-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "66410f0d41a41a6360e74ce1cf4aa3fdb5be7ff8e096ff8be94f4d2f8319f3a6"
+      url "https://github.com/neuralshyam/kport/releases/download/v0.2.1/kport-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3bf6a9066d67589f68834cd142d8ce3f95f59afdb9be702d79fc71d73df25105"
     else
-      url "https://github.com/neuralshyam/kport/releases/download/v0.2.0/port-killer-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "32b986900b96cb35c65c342f0d9c490a02ef495fa0451a24896fe2225e365cb9"
+      url "https://github.com/neuralshyam/kport/releases/download/v0.2.1/kport-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "fa4b6f13caa961a773e1dd48c28bf40c0953e929394efb12291247359be49d68"
     end
   end
 
   def install
-    bin.install "port-killer" => "kport"
+    bin.install "kport"
     bin.install_symlink bin/"kport" => "port-killer"
   end
 
